635b462b53f885f9635f09fa5b04436c682cb318
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2021-12-27 13:23:59 +0100

    consultation-view terminata

--- src/main/java/it/ispw/booknook/ConsultationUIController.java
+++ src/main/java/it/ispw/booknook/ConsultationUIController.java
@@ -22,52 +22,47 @@ import javafx.scene.text.Font;
 import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Popup;
 
 
 public class ConsultationUIController {
 
+    private ObservableList<String> items;
 
-    @FXML
-    void onSearchClick(MouseEvent event) {
-
+    private void createListLibraries(ObservableList<String> items, MouseEvent event, Parent root) {
         //compare la lista delle biblioteche con disponiilità
         ListView<String> libraries = new ListView<>();
-        //recupera librerie con disponibilità e le inserisce nella lista view
-        ObservableList<String> items = FXCollections.observableArrayList("Primo", "Secondo", "Terzo", "Quarto",
-                "Quinto", "Sesto", "Settimo", "Ottavo");
         libraries.setItems(items);
         libraries.setPrefHeight(136);
         libraries.setPrefWidth(377);
-        ImageView searchIcon = (ImageView) event.getSource();
-        AnchorPane anchorPane = (AnchorPane) searchIcon.getParent();
-        BorderPane borderPane = (BorderPane) anchorPane.getParent();
-        AnchorPane paneCenter = (AnchorPane) borderPane.getCenter();
-        paneCenter.getChildren().add(libraries);
         libraries.setLayoutX(475);
         libraries.setLayoutY(21);
         libraries.setStyle("-fx-focus-color: transparent;");
         libraries.setFixedCellSize(43);
         libraries.getStylesheets().add(Objects.requireNonNull(getClass().getResource("list.css")).toExternalForm());
-        paneCenter.setVisible(true);
+        AnchorPane pane = (AnchorPane)root.lookup("#listPane");
+        pane.getChildren().add(libraries);
+        pane.setVisible(true);
 
         //cambia schermata quando clicco sulla libreria -->
         libraries.setOnMouseClicked(event1 -> {
 
             System.out.println("clicked on " + libraries.getSelectionModel().getSelectedItem());
-            Parent root = null;
+            Parent newRoot = null;
             try {
-                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("consultationdetails-view.fxml")));
+                newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("consultationdetails-view.fxml")));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Scene scene = ((ImageView)(event.getSource())).getScene();
-            scene.setRoot(root);
-            assert root != null;
-            root.requestFocus();
+            Scene scene = ((ListView)(event1.getSource())).getScene();
+            scene.setRoot(newRoot);
+            assert newRoot != null;
+            newRoot.requestFocus();
             WebView webView = (WebView)scene.lookup("#map");
             webView.getEngine().load("https://www.google.com/maps");
 
@@ -82,6 +77,18 @@ public class ConsultationUIController {
                 }
             });
         });
+
+    }
+
+
+
+    @FXML
+    void onSearchClick(MouseEvent event) {
+        //recupera librerie con disponibilità e le inserisce nella lista view
+        items = FXCollections.observableArrayList("Primo", "Secondo", "Terzo", "Quarto",
+                "Quinto", "Sesto", "Settimo", "Ottavo");
+
+        createListLibraries(items, event, ((ImageView)(event.getSource())).getScene().getRoot());
     }
 
 
@@ -101,9 +108,47 @@ public class ConsultationUIController {
         DatePicker picker = (DatePicker)event.getSource();
         ListView<String> timeTable = (ListView<String>) picker.getScene().lookup("#timeTable");
         //mostra soltanto gli orari disponibili
-        ObservableList<String> items = FXCollections.observableArrayList("8.00", "9.30", "10.15", "10.30",
+        ObservableList<String> times = FXCollections.observableArrayList("8.00", "9.30", "10.15", "10.30",
                 "11.00", "11.30", "15.15", "16.30");
-        timeTable.setItems(items);
+        timeTable.setItems(times);
+    }
+
+
+
+    @FXML
+    void onBackClick(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("consultation-view.fxml")));
+        Scene scene = ((ImageView)(event.getSource())).getScene();
+        scene.setRoot(root);
+        root.requestFocus();
+        items = FXCollections.observableArrayList("Primo", "Secondo", "Terzo", "Quarto",
+                "Quinto", "Sesto", "Settimo", "Ottavo");
+        createListLibraries(items, event, root);
+    }
+
+    @FXML
+    void onConsultationClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("consultation-view.fxml")));
+        Scene scene = ((Button)(event.getSource())).getScene();
+        scene.setRoot(root);
+        root.requestFocus();
+    }
+
+    @FXML
+    void onConfirmClick(ActionEvent event) {
+        System.out.println("Click");
+        //compare il popup in caso di successo
+        Popup popup = new Popup();
+        Label label = new Label("This is a Popup");
+        label.setMinWidth(80);
+        label.setMinHeight(50);
+        label.setStyle(" -fx-background-color: white;");
+        popup.getContent().add(label);
+
+
+        popup.show(((Button)event.getSource()).getScene().getWindow());
+
+
     }
 
 }
