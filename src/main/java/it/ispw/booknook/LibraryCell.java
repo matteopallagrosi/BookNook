6ff5aded22492e09745c9e28ffe4cd2e19faf62e
simonettaricci <simonetta.ricci@outlook.com>
2022-01-07 17:23:38 +0100

    reservation dialogue

--- src/main/java/it/ispw/booknook/LibraryCell.java
+++ src/main/java/it/ispw/booknook/LibraryCell.java
@@ -4,11 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Random;
 
 public class LibraryCell {
@@ -41,7 +44,7 @@ public class LibraryCell {
     }
 
     private void setBtnHandler() {
-        borrowBtn.setOnAction(new EventHandler<ActionEvent>() {
+        borrowBtn.setOnAction(new EventHandler<>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 if (borrowBtn.getText().equals("Borrow")) {
@@ -51,6 +54,36 @@ public class LibraryCell {
                 else {
                     System.out.println("Reserve");
                     //apre dialog per avvenuta reservation e notifica alla biblioteca
+                    borrowBtn.setOnMouseClicked(mouseEvent -> {
+                        Dialog<ButtonType> dialog = new Dialog<>();
+                        dialog.setTitle("Confirm reservation");
+                        ButtonType type = new ButtonType("Done", ButtonBar.ButtonData.OK_DONE);
+                        dialog.setContentText("The librarian has received your reservation,\nyou are queuing now.\nThank you!");
+                        dialog.getDialogPane().setStyle("-fx-font-size: 15px;" +
+                                "-fx-font-family: Roboto ");
+                        dialog.getDialogPane().getButtonTypes().add(type);
+                        ButtonBar buttonBar = (ButtonBar)dialog.getDialogPane().lookup(".button-bar");
+                        buttonBar.getButtons().forEach(b -> b.setStyle("-fx-background-color: #e9bf8e;" +
+                                "-fx-background-radius: 8;" +
+                                "-fx-effect: dropshadow(one-pass-box, rgba(0,0,0,0.5), 10, 0, 0, 2);" +
+                                "-fx-text-fill: white;" +
+                                "-fx-font-family: 'Roboto Medium'"));
+
+                        Optional<ButtonType> result = dialog.showAndWait();
+                        if (result.isPresent() && result.get() == type) {
+                            //conferma la prenotazione, invia email, riporta alla schermata iniziale
+                            Parent root = null;
+                            try {
+                                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("homepage-view.fxml")));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Scene scene = ((Button)(mouseEvent.getSource())).getScene();
+                            scene.setRoot(root);
+                            assert root != null;
+                            root.requestFocus();
+                        }
+                    });
                 }
 
             }
