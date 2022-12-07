848ef7872a833726b4d8854277e15382e47cd9d1
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2022-12-07 12:27:55 +0100

    aggiunta mappa funzionante

--- src/main/java/it/ispw/booknook/logic/boundary/main_view/BorrowDetailsUIController.java
+++ src/main/java/it/ispw/booknook/logic/boundary/main_view/BorrowDetailsUIController.java
@@ -47,8 +47,8 @@ public class BorrowDetailsUIController extends UIController implements Initializ
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ArcGISRuntimeEnvironment.setInstallDirectory("C:\\Users\\HP\\.arcgis\\100.15.0");
-        String yourApiKey = "AAPK26b105d18af3457a9265837b9abf295b5q_qht8-El1IQE4HeVuW13Owo8VQXOzKJwPJ9Bu4e_S9EO7YYER8Jn20YUSNe2lN";
-        ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
+        String mario = "AAPK26b105d18af3457a9265837b9abf295b5q_qht8-El1IQE4HeVuW13Owo8VQXOzKJwPJ9Bu4e_S9EO7YYER8Jn20YUSNe2lN";
+        ArcGISRuntimeEnvironment.setApiKey(mario);
 
         // create a MapView to display the map and add it to the stack pane
         mapView = new MapView();
